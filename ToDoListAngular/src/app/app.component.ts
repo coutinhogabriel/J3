025e8453fea6f8e6ea9d4153e259c6c0c93bf7b4
coutinhogabriel <gabriel.coutin@hotmail.com>
2024-02-07 21:42:31 -0300

    07/02

--- ToDoListAngular/src/app/app.component.ts
+++ ToDoListAngular/src/app/app.component.ts
@@ -1,13 +1,18 @@
-import { Component } from '@angular/core';
+import { Component, NgModule } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { HeaderComponent } from "../header/header.component";
+import { FooterComponent } from "../footer/footer.component";
+import { HttpClientModule } from '@angular/common/http';
+import { FormsModule } from '@angular/forms';
 
 @Component({
-  selector: 'app-root',
-  standalone: true,
-  imports: [RouterOutlet],
-  templateUrl: './app.component.html',
-  styleUrl: './app.component.css'
+    selector: 'app-root',
+    standalone: true,
+    templateUrl: './app.component.html',
+    styleUrl: './app.component.css',
+    imports: [RouterOutlet, HeaderComponent, FooterComponent,HttpClientModule,FormsModule]
 })
 export class AppComponent {
   title = 'ToDoListAngular';
+usuario: any;
 }
